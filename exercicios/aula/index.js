b16483c33b9d56fad8af987ab194aca7a3b463cc
Ana Beatriz Morita <anabemorita@gmail.com>
2023-04-29 11:18:23 -0300

    Adicionar explicacoes lacos

---
+++ exercicios/aula/index.js
@@ -0,0 +1,25 @@
+let emailDigitado = prompt('Digite um e-mail');
+let emailCadastrado = 'on23@reprograma.com.br';
+while (emailDigitado !== emailCadastrado) {
+    alert('Email não encontrado')
+    emailDigitado = prompt('Digite novamente o e-mail')
+}
+
+// let emailDigitado = prompt('Digite um e-mail');
+// let emailCadastrado = 'on23@reprograma.com.br';
+if (emailDigitado !== emailCadastrado) {
+    alert('Email não encontrado')
+    emailDigitado = prompt('Digite novamente o e-mail')
+}
+if (emailDigitado !== emailCadastrado) {
+    alert('Email não encontrado')
+    emailDigitado = prompt('Digite novamente o e-mail')
+}
+if (emailDigitado !== emailCadastrado) {
+    alert('Email não encontrado')
+    emailDigitado = prompt('Digite novamente o e-mail')
+}
+if (emailDigitado !== emailCadastrado) {
+    alert('Email não encontrado')
+    emailDigitado = prompt('Digite novamente o e-mail')
+}
