b3715769cd5e0b8c2da6e86617dcdaa83cc0e7f9
amanddmouraz <amanlsmoura@gmail.com>
2023-05-02 20:21:28 -0300

    alterações Amanda

---
+++ exercicios/aula/exercicio1.js
@@ -0,0 +1,35 @@
+// // let emailCadastrado = "anabia@repro.com.br";
+// // let senhaCadastrada = "teste123";	
+
+// // let emailDigitado = prompt("Qual o seu e-mail?");
+// // let senhaDigitada = prompt("Qual a sua senha?");
+
+// // while (emailCadastrado !== emailDigitado || senhaCadastrada  !==  senhaDigitada) {
+// //     alert("Informações de login incorretas");
+// //     senhaDigitada = prompt("Digite novamente a sua senha");
+// //     emailDigitado = prompt("Digite seu e-mail novamente");
+// // };
+// // alert("Acertou mizeravi")
+
+
+// let listaDeCompras = [["leite", "biscoito", "melancia"],
+// ["banana", "maça", "ovo"]]
+
+
+// // console.log(listaDeCompras[0][0], listaDeCompras[0][2])
+
+// // console.log(listaDeCompras.length)
+
+// // for (let i = 0; i < listaDeCompras.length; i++) {
+// //     console .log(listaDeCompras[i]);
+// //   }  splice -> remove o item de vdd / slice -> só faz uma cópia sem o item
+
+// //   listaDeCompras[0].push("Coquinha")
+
+//   listaDeCompras[0].splice(0, 1) 
+
+// //   0 -> de onde começa
+// //   1 -> a quantidade de item que será retirado
+
+//   console.log(listaDeCompras)
+
---
+++ exercicios/aula/exercicio2.js
@@ -0,0 +1,23 @@
+// OBJETOS
+
+
+let pessoas = {
+    nome: "Amanda",
+    idade: 24,
+    ehDev: true,
+    animesFav: ["Onepiece", "jujutso", "black clover"]
+};
+
+
+console.log(pessoas.animesFav[2])
+
+
+// Object.keys(pessoas) -> contar os itens de um objeto
+
+// Para alterar/trocar itens de um objeto basta acessar o objeto.chave = alteração. O mesmo para adicionar! 
+// Exemplo:
+
+pessoas.sobrenome = "Moura"
+
+
+console.log(pessoas)
