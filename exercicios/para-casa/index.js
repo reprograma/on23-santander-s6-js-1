9307d9069c2ea5a8d173ee2a8a9923d5b66a196b
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2023-05-03 19:42:31 -0300

    Exercicio concluido com sucesso!

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,11 +1,3 @@
 // QUESTÃO: Faça uma função que recebe o preço de um produto e o percentual de
 // desconto a ser dado no produto e retorne o valor do produto com desconto.
 
-// let valorDoProduto = document.querySelector('#valorDoProduto');
-// let percentualDeDesconto = document.querySelector('#percentualDeDesconto');
-// let desconto = document.querySelector('span');
-// let valorFinalDaCompra = document.querySelector('p')
-
-// function aplicandoDesconto() {
-//     desconto.innerHTML = parseInt(valorDoProduto.value) * parseInt(percentualDeDesconto.value) / 100;
-// } 
