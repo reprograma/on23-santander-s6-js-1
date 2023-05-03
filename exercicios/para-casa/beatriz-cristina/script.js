47cd21ec0bff68dbbaa72c7d02d3f1bc2fe06498
Beatriz Cristina <beatrizcristina.dev@gmail.com>
2023-05-03 20:33:15 -0300

    Entendi o que p retrurn faz

--- exercicios/para-casa/beatriz-cristina/script.js
+++ exercicios/para-casa/beatriz-cristina/script.js
@@ -24,9 +24,9 @@ let percentualDesconto = prompt("E a porcentagem do desconto?")
 function calcularPrecoComDesconto (preco, desconto) {
     let calculoDesconto = preco * desconto / 100;
     let precoComDesconto = preco - calculoDesconto;
-    // return precoComDesconto;
     alert("O preço final do produto é: " + precoComDesconto);
     console.log("O preço final do produto é: " + precoComDesconto);
+    return ("O preço final do produto é: " + precoComDesconto);
 }
 
 calcularPrecoComDesconto (precoProduto, percentualDesconto)
