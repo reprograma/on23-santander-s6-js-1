ba692848e14534d05cdfbee998de1209cdd8d323
Beatriz Cristina <beatrizcristina.dev@gmail.com>
2023-05-03 10:03:00 -0300

    teste return

--- exercicios/para-casa/beatriz-cristina/script.js
+++ exercicios/para-casa/beatriz-cristina/script.js
@@ -18,14 +18,15 @@
 // 7- alert/ console.log
 
 
-let precoProduto = prompt("Insira o preço do produto.")
-let percentualDesconto = prompt("Insara a porcentagem de desconto.")
+let precoProduto = prompt("Qual é o preço do produto?")
+let percentualDesconto = prompt("E a porcentagem do desconto?")
 
-function calcularPrecoComDesconto (preco , desconto) {
+function calcularPrecoComDesconto (preco, desconto) {
     let calculoDesconto = preco * desconto / 100;
     let precoComDesconto = preco - calculoDesconto;
-    alert(precoComDesconto);
-    console.log(precoComDesconto);
+    // return precoComDesconto;
+    alert("O preço final do produto é: " + precoComDesconto);
+    console.log("O preço final do produto é: " + precoComDesconto);
 }
 
 calcularPrecoComDesconto (precoProduto, percentualDesconto)
